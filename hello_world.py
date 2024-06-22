5a1a874f8d76a4be7a715bf2518f4713ca89823d
shirlynclare <168082416+shirlynclare@users.noreply.github.com>
2024-06-22 16:41:23 +0300

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+from asyncflows import AsyncFlows
+
+import asynflows 
+async def main():
+    # Load the flow from the file
+    flow = AsyncFlows.from_file("hello_world.yaml")
+
+    # Run the flow
+    result = await flow.set_vars(name="MY_NAME").run()
+
+    # Print the result
+    print(result)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
